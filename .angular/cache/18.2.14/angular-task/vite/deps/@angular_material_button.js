23e68d1d3641f44ade0d530d5e3033575b608fc6
Alexander Ershov <lancaster1@ro.ru>
2025-03-10 10:45:37 +0300

    last fix project

---
+++ .angular/cache/18.2.14/angular-task/vite/deps/@angular_material_button.js
@@ -0,0 +1,33 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-2UONRB32.js";
+import "./chunk-FOLBAVSS.js";
+import "./chunk-BBS7MU37.js";
+import "./chunk-WN35FCBX.js";
+import "./chunk-S35MAB2V.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
---
+++ .angular/cache/18.2.14/angular-task/vite/deps/@angular_material_input.js
@@ -0,0 +1,33 @@
+import {
+  MAT_INPUT_VALUE_ACCESSOR,
+  MatInput,
+  MatInputModule,
+  getMatInputUnsupportedTypeError
+} from "./chunk-BZUBQ4BE.js";
+import "./chunk-AXGSL2YI.js";
+import {
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix
+} from "./chunk-PX5I6EAL.js";
+import "./chunk-UZRB7HGO.js";
+import "./chunk-FOLBAVSS.js";
+import "./chunk-BBS7MU37.js";
+import "./chunk-WN35FCBX.js";
+import "./chunk-S35MAB2V.js";
+export {
+  MAT_INPUT_VALUE_ACCESSOR,
+  MatError,
+  MatFormField,
+  MatHint,
+  MatInput,
+  MatInputModule,
+  MatLabel,
+  MatPrefix,
+  MatSuffix,
+  getMatInputUnsupportedTypeError
+};
+//# sourceMappingURL=@angular_material_input.js.map
